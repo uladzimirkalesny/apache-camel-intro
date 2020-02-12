527ad132920dcb1ef827b89f8a12b96c4e130c03
ukalesny <u.kalesny@andersenlab.com>
2020-02-12 18:01:02 +0300

    Initial commit

---
+++ src/main/java/examples/intro/FileCopierWithCamel.java
@@ -0,0 +1,21 @@
+package examples.intro;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class FileCopierWithCamel {
+    public static void main(String[] args) throws Exception {
+        CamelContext camelContext = new DefaultCamelContext();
+        camelContext.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() {
+                from("file:data/inbox?noop=true").to("file:data/outbox");
+            }
+        });
+
+        camelContext.start();
+        Thread.sleep(10000);
+        camelContext.stop();
+    }
+}
---
+++ src/main/java/examples/intro/FileCopierWithSpring.java
@@ -0,0 +1,16 @@
+package examples.intro;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.spring.SpringCamelContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class FileCopierWithSpring {
+    public static void main(String[] args) throws Exception {
+        ApplicationContext springContext = new ClassPathXmlApplicationContext("classpath:app-context.xml");
+        CamelContext camelContext = new SpringCamelContext(springContext);
+        camelContext.start();
+        Thread.sleep(1000);
+        camelContext.close();
+    }
+}
